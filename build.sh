18ce73642e31bf1b4e4d5b130f61996368ac6671
GHTaarn <62629455+GHTaarn@users.noreply.github.com>
2022-09-24 09:02:55 +0200

    Release publishing instructions in build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
+#!/bin/sh
+# To add a Julia version to the repository, do the following:
+#
+#  1. Generate the .deb files (e.g. with juliapt)
+#  2. Copy (or move) the .deb files to docs/stable or docs/lts as appropriate
+#  3. Run this script with:
+#    ./build.sh
+#  4. Add the changes to git with:
+#     git add docs; git commit
+#  5. To publish on Github just push the changes with:
+#     git push
 cd docs
 dpkg-scanpackages --multiversion stable /dev/null | gzip -9c > stable/Packages.gz
 dpkg-scanpackages --multiversion lts /dev/null | gzip -9c > lts/Packages.gz
